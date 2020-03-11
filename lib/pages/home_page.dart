b5f369f6c3b55de46d4081494ab8816cbdc9f079
wangjunwei <zhengeili@qq.com>
2020-03-11 20:30:36 +0800

    code05

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    getHttp();
     return Scaffold(
       body:Center(child:Text('商城首页'))
     );
+  }
+
+  void getHttp() async {
+    try {
+      Response reponse;
+      reponse=await Dio().get('https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=大胸美女');
+      return print(reponse);
+    } catch (e) {
+      print(e);
+    }
   }
 }
