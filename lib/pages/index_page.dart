0430f5928797a58d45f7dd2fc0198bcd3d995662
wangjunwei <zhengeili@qq.com>
2020-03-11 08:52:09 +0800

    code03

--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
-class IndexPage extends StatelessWidget {
+class IndexPage extends StatefulWidget {
+  IndexPage({Key key}) : super(key: key);
+
+  @override
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  final List<BottomNavigationBarItem> bottomTabs=[
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.home),//IOS风格的
+      title: Text('首页')
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.search),
+      title: Text('分类')
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.shopping_cart),
+      title: Text('购物车')
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.profile_circled),
+      title: Text('会员中心')
+    )
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title:Text('百姓生活+')),
-      body: Center(
-        child: Text('百姓生活+'),
-      ),
+    return Container(
+       //child: child,
     );
   }
 }
