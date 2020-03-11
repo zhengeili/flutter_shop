41a95636d357a557065acef3b88cb787fd36551f
wangjunwei <zhengeili@qq.com>
2020-03-11 22:21:47 +0800

    code07请求头的设置

---
+++ lib/config/httpHeaders.dart
@@ -0,0 +1,12 @@
+const httpHeaders={
+  'Accept': 'application/json, text/plain',
+'Accept-Encoding': 'gzip, deflate',
+'Accept-Language': 'zh-CN,zh;q=0.9',
+'Connection': 'keep-alive',
+'Content-Type': 'application/json',
+'Cookie': '_ga=GA1.2.381584897.1534769652; Hm_lvt_022f847c4e3acd44d4a2481d9187f1e6=1552920445,1553872955,1553873168,1554093939; _gid=GA1.2.335717019.1554093939; SERVERID=3431a294a18c59fc8f5805662e2bd51e|1554094392|1554093939; Hm_lpvt_022f847c4e3acd44d4a2481d9187f1e6=1554094392; _gat=1',
+'Host': 'time.geekbang.org',
+'Origin': 'https://time.geekbang.org',
+'Referer': 'https://time.geekbang.org/',
+'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
+};
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import '../config/httpHeaders.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,66 +8,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController typeController=TextEditingController();
-  String showText='欢迎您来到美好人间高级会所';
+  String showText='还没有请求数据';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:Text('美好人间')),
+      appBar: AppBar(title:Text('请求远程数据')),
       body: SingleChildScrollView(
-        child:Container(
         child: Column(
           children: <Widget>[
-            TextField(
-              controller: typeController,
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.all(10.0),
-                labelText: '美女类型',//在文本框的上面显示
-                helperText: '请输入你喜欢的类型',//在文本框下面显示的
-              ),
-              autofocus: false,//默认不打开手机的输入键盘，自动对焦
-            ),
             RaisedButton(
-              onPressed: _choiceAction,
-              child: Text('选择完毕'),
+              onPressed: _jike,
+              child: Text('请求数据'),
             ),
-            Text(
-              showText,
-              overflow: TextOverflow.ellipsis,//超出长度显示省略号
-              maxLines: 1,//最多显示一行文本
-            )
+            Text(showText)
           ],
-        )
+        ),
       ),
-      )
     );
   }
-  void _choiceAction(){
-    print('开始选择你喜欢的类型.......');
-    if(typeController.text.toString()==''){
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(title: Text('美女类型不能为空'),)
-      );
-    }else{
-      getHttp(typeController.text.toString()).then((val){
-        setState(() {
-          showText=val['data']['name'].toString();
-        });
+  void _jike(){
+    print('开始向极客时间请求数据......');
+    getHttp().then((val){
+      setState(() {
+        showText=val['data'].toString();
       });
-    }
+    });
   }
-  Future getHttp(String typeText) async {
+  Future getHttp() async{
     try {
       Response response;
-      var data={'name':typeText};
-      response=await Dio().post('https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/post_dabaojian',
-      queryParameters: data
-      );
+      Dio dio=new Dio();
+      dio.options.headers=httpHeaders;//设置请求头
+      response=await dio.get('https://time.geekbang.org/serv/v1/column/newAll');
+      print(response);
       return response.data;
     } catch (e) {
-      print('异常信息：'+e);
+      return print(e);
     }
-    
   }
 }
