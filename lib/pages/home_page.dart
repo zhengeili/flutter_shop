0cefc8e6788d2516b25b784e0eb1b0bdf54b559f
wangjunwei <zhengeili@qq.com>
2020-03-12 12:46:01 +0800

    code10

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../service/service_method.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,23 +10,46 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String homePageContent='正在获取数据';
-  @override
-  void initState() {
-    getHomePageContent().then((val){
-        setState(() {
-          homePageContent=val.toString();
-        });
-    });
-    super.initState();
-  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:Text('请求远程数据')),
-      body: SingleChildScrollView(
-        child: Text(homePageContent)
-      ),
+      appBar: AppBar(title:Text('百兴生活+')),
+      body: FutureBuilder(
+        future: getHomePageContent(),
+        builder: (context,snapshot){
+          if(snapshot.hasData){
+            var data=json.decode(snapshot.data.toString());
+            List<Map> swiper=(data['data']['slides'] as List).cast();
+            return Column(
+              children: <Widget>[
+                SwiperDiy(swiperDateList: swiper)
+              ],
+            );
+          }
+        }
+      )
     );
   }
   
+}
+
+class SwiperDiy extends StatelessWidget {
+  final List swiperDateList;
+  SwiperDiy({this.swiperDateList});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 333,
+      child: Swiper(
+        itemBuilder: (BuildContext context,int index){
+          return Image.network("${swiperDateList[index]['image']}",fit: BoxFit.fill);
+        },
+        itemCount: swiperDateList.length,
+        pagination: SwiperPagination(),
+        autoplay: true,
+      ),
+    );
+  }
 }
